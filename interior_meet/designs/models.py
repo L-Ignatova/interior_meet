95528e0f28f9cbba2dd4573fad2266222b13b30d
Liliya Ignatova <liliya.oignatova@gmail.com>
2021-07-24 14:16:29 +0300

    add Design model

--- interior_meet/designs/models.py
+++ interior_meet/designs/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Design(models.Model):
+    title = models.CharField(
+        max_length=10,
+    )
+    city = models.CharField(
+        max_length=15,
+    )
+    country = models.CharField(
+        max_length=20,
+    )
+    description = models.TextField()
+    image = models.ImageField(
+        upload_to='designs',
+    )
+
+
+class Like(models.Model):
+    design = models.ForeignKey(
+        Design,
+        on_delete=models.CASCADE,
+    )
