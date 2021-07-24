b90be454ad4092fa04e3aeef4770a776ffa7881e
Liliya Ignatova <liliya.oignatova@gmail.com>
2021-07-24 15:00:45 +0300

    update views

--- interior_meet/designs/views.py
+++ interior_meet/designs/views.py
@@ -2,10 +2,5 @@ from django.shortcuts import render
 
 
 # temporary
-def landing_page(req):
-    return render(req, 'home_page.html')
-
-
-# temporary
 def add_page(req):
     return render(req, 'add.html')
